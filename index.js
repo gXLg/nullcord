273f52b9ee27349da91205d7e330a78935ffc985
gXLg <natgaev@gmail.com>
2023-04-30 12:18:23 +0200

    consts added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Bot } = require("./lib/ds.js");
 const utils = require("./lib/utils.js");
+const consts = require("./lib/consts.js");
 
-module.exports = { Bot, utils };
+module.exports = { Bot, utils, consts };
